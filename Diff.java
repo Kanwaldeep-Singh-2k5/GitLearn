56707926697f755b72757d528ce39778789c6f01
Kanwaldeep Singh <kanwalshehry@gmail.com>
2024-10-03 12:40:30 +0530

    added implementation for Diff.java

--- Diff.java
+++ Diff.java
@@ -1,5 +1,8 @@
 public class Diff{
     public static void main(String [] args){
-        
+      int a=0;
+      int b=0;
+      int c=a-b;
+        System.out.println("Difference is:"+c);
     }
 }
